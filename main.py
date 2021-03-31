6d4fd7b518a5b4aead5a729acd394a3ede0f2a19
Tapabrata Dey <tapabratadey02@gmail.com>
2021-03-30 23:25:32 -0400

    testing python

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Tapabrata Dey #001521455
+
+def welcome_msg():
+    print("\nWelcome to Parcel Delivery Service")
+    print("----------------------------------\n")
+
+
+def display_total_distance_traveled():
+    print("Total mileage traveled by all trucks : [X] miles\n")
+
+
+def main():
+    welcome_msg()
+    display_total_distance_traveled()
+
+
+if __name__ == "__main__":
+    main()
