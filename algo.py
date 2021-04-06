e43c8d5fcc1b0ecf9a0e17633c1f60061d40ac94
Tapabrata Dey <tapabratadey02@gmail.com>
2021-04-06 04:43:42 -0400

    formatting

--- algo.py
+++ algo.py
@@ -12,73 +12,73 @@ third_deliv_sorted = []
 third_deliv_sorted_idx = []
 
 def fastest_route(delivery_list, delivery_num, current_loc, csv_address_data):
-	if (len(delivery_list) == 0):
-		return delivery_list
-	else:
-		try:			
-			min_dist = 40.0
-			new_loc = 0
-			curr_dist = 0
-			find_min_dist(delivery_list,
-										delivery_num,
-										current_loc,
-										csv_address_data,
-										min_dist,
-										new_loc,
-										curr_dist)
-		except IndexError:
-			pass
+  if (len(delivery_list) == 0):
+    return delivery_list
+  else:
+    try:			
+      min_dist = 30.0
+      new_loc = 0
+      curr_dist = 0
+      find_min_dist(delivery_list,
+                    delivery_num,
+                    current_loc,
+                    csv_address_data,
+                    min_dist,
+                    new_loc,
+                    curr_dist)
+    except IndexError:
+      pass
 
 def find_min_dist(delivery_list,
-									delivery_num,
-									current_loc,
-									csv_address_data,
-									min_dist,
-									new_loc,
-									curr_dist):
-		for i in range(len(delivery_list)):
-			temp = find_current_dist_val(current_loc,
-											delivery_list[i].address_location,
-											csv_address_data)
-			if temp <= min_dist:
-				min_dist = temp
-				new_loc = delivery_list[i].address_location
-		for i in range(len(delivery_list)):
-			temp = find_current_dist_val(current_loc,
-											delivery_list[i].address_location,
-											csv_address_data)
-			if (temp == min_dist):
-				if (delivery_num == "first"):
-					sort_first_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
-				elif (delivery_num == "second"):
-					sort_second_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
-				elif (delivery_num == "third"):															
-					sort_third_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
+                  delivery_num,
+                  current_loc,
+                  csv_address_data,
+                  min_dist,
+                  new_loc,
+                  curr_dist):
+    for i in range(len(delivery_list)):
+      temp = find_current_dist_val(current_loc,
+                      delivery_list[i].address_location,
+                      csv_address_data)
+      if temp <= min_dist:
+        min_dist = temp
+        new_loc = delivery_list[i].address_location
+    for i in range(len(delivery_list)):
+      temp = find_current_dist_val(current_loc,
+                      delivery_list[i].address_location,
+                      csv_address_data)
+      if (temp == min_dist):
+        if (delivery_num == "first"):
+          sort_first_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
+        elif (delivery_num == "second"):
+          sort_second_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
+        elif (delivery_num == "third"):															
+          sort_third_delivery(delivery_list, i, current_loc, new_loc, csv_address_data)
 
 def sort_first_delivery(delivery_list, i, current_loc, new_loc, csv_address_data):
-			first_deliv_sorted.append(delivery_list[i])
-			first_deliv_sorted_idx.append(delivery_list[i].address_location)
-			delivery_list.pop(delivery_list.index(delivery_list[i]))
-			current_loc = new_loc
-			fastest_route(delivery_list, "first", current_loc, csv_address_data)
+      # first_deliv_sorted.append(delivery_list[i])
+      first_deliv_sorted_idx.append(delivery_list[i].address_location)
+      delivery_list.pop(delivery_list.index(delivery_list[i]))
+      current_loc = new_loc
+      fastest_route(delivery_list, "first", current_loc, csv_address_data)
 
 def sort_second_delivery(delivery_list, i, current_loc, new_loc, csv_address_data):
-			second_deliv_sorted.append(delivery_list[i])
-			second_deliv_sorted_idx.append(delivery_list[i].address_location)
-			delivery_list.pop(delivery_list.index(delivery_list[i]))
-			current_loc = new_loc
-			fastest_route(delivery_list, "second", current_loc, csv_address_data)
+      # second_deliv_sorted.append(delivery_list[i])
+      second_deliv_sorted_idx.append(delivery_list[i].address_location)
+      delivery_list.pop(delivery_list.index(delivery_list[i]))
+      current_loc = new_loc
+      fastest_route(delivery_list, "second", current_loc, csv_address_data)
 
 def sort_third_delivery(delivery_list, i, current_loc, new_loc, csv_address_data):
-			third_deliv_sorted.append(delivery_list[i])
-			third_deliv_sorted_idx.append(delivery_list[i].address_location)
-			delivery_list.pop(delivery_list.index(delivery_list[i]))
-			current_loc = new_loc
-			fastest_route(delivery_list, "third", current_loc, csv_address_data)
+      # third_deliv_sorted.append(delivery_list[i])
+      third_deliv_sorted_idx.append(delivery_list[i].address_location)
+      delivery_list.pop(delivery_list.index(delivery_list[i]))
+      current_loc = new_loc
+      fastest_route(delivery_list, "third", current_loc, csv_address_data)
 
 
 def find_current_dist_val(row, col, csv_address_data):
-	curr_dist = csv_address_data[row][col]
-	if curr_dist == '':
-		curr_dist = csv_address_data[col][row]
-	return float(curr_dist)
+  curr_dist = csv_address_data[row][col]
+  if curr_dist == '':
+    curr_dist = csv_address_data[col][row]
+  return float(curr_dist)
--- package_handler.py
+++ package_handler.py
@@ -14,19 +14,24 @@ third_del_sTime = dt.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
 
 # updating start time of packages
 def updt_first_del_sTime(package):
-		package.start_time = first_del_sTime
+    package.start_time = first_del_sTime
 def updt_second_del_sTime(package):
-		package.start_time = second_del_sTime
+    package.start_time = second_del_sTime
 def updt_third_del_sTime(package):
-		package.start_time = third_del_sTime
+    package.start_time = third_del_sTime
 
+# updating package locations
+# O(n^2)
 def updt_pkg_loc(delivery_list, csv_address):
-	for i in range(len(delivery_list)):
-		for j in range(len(csv_address)):
-			if (delivery_list[i].address == csv_address[j][2]):
-				delivery_list[i].address_location = int(csv_address[j][0])
+  for i in range(len(delivery_list)):
+    for j in range(len(csv_address)):
+      if (delivery_list[i].address == csv_address[j][2]):
+        delivery_list[i].address_location = int(csv_address[j][0])
 
+# updating sorted list for each delivery.
+# setting the starting point to from location 0
 def updt_starting_loc():
-	algo.first_deliv_sorted_idx.insert(0, '0')
-	algo.second_deliv_sorted_idx.insert(0, '0')
-	algo.third_deliv_sorted_idx.insert(0, '0')
+  algo.first_deliv_sorted_idx.insert(0, '0')
+  algo.second_deliv_sorted_idx.insert(0, '0')
+  algo.third_deliv_sorted_idx.insert(0, '0')
+  
